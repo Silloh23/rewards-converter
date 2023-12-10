ed3e656534b95dc2bdb69d3f90e807bc7ef77614
Silloh23 <116902188+Silloh23@users.noreply.github.com>
2023-12-10 20:10:36 +0000

    Update final 2 tests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,22 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100.0;
+        double expectedMilesValue = cashValue / 0.0035;
+
+        var rewardValue = new RewardValue(cashValue);
+        double actualMilesValue = rewardValue.getMilesValue();
+
+        assertEquals(expectedMilesValue, actualMilesValue, 0.001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCashValue = milesValue / 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        double actualCashValue = rewardValue.getCashValue();
+
+        assertEquals(expectedCashValue, actualCashValue);
     }
 }
